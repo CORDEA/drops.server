a6e7cf7e02a9b788c25528a34754c74eb0666c34
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-14 09:44:53 +0900

    define new endpoints

--- items.go
+++ items.go
@@ -3,5 +3,10 @@ package main
 import "github.com/gin-gonic/gin"
 
 func addItemRoutes(g *gin.Engine) {
-	_ = g.Group("/items")
+	items := g.Group("/items")
+
+	items.GET("/", func(context *gin.Context) {
+	})
+	items.GET("/:id", func(context *gin.Context) {
+	})
 }
--- user.go
+++ user.go
@@ -3,5 +3,20 @@ package main
 import "github.com/gin-gonic/gin"
 
 func addUserRoutes(g *gin.Engine) {
-	_ = g.Group("/user")
+	user := g.Group("/user")
+
+	user.POST("/login", func(context *gin.Context) {
+	})
+	user.POST("/new", func(context *gin.Context) {
+	})
+	user.GET("/achievements", func(context *gin.Context) {
+	})
+	user.GET("/cart", func(context *gin.Context) {
+	})
+	user.POST("/cart", func(context *gin.Context) {
+	})
+	user.GET("/orders", func(context *gin.Context) {
+	})
+	user.PATCH("/orders/cancel/:id", func(context *gin.Context) {
+	})
 }
