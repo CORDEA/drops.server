e109b244ad271ef2e442b4b516516c65cbf68c59
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-17 15:09:17 +0900

    create client

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"drops/etsy"
 	"drops/items"
 	"drops/user"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 var router = gin.Default()
 
 func main() {
+	key := os.Getenv("ETSY_API_KEY")
+	client := etsy.NewClient(key)
 	user.AddRoutes(router)
 	items.AddRoutes(router)
 	router.Run()
