041a9179d2b07ed5d02b9ca94ef96bf516e840cb
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-18 09:13:04 +0900

    update response

--- items/items.go
+++ items/items.go
@@ -10,10 +10,25 @@ type Items struct {
 }
 
 type Item struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Price    int    `json:"price"`
-	ImageUrl string `json:"image_url"`
+	Id                 string   `json:"id"`
+	Name               string   `json:"name"`
+	Description        string   `json:"description"`
+	ImageUrl           string   `json:"image_url"`
+	CreatedAt          float32  `json:"created_at"`
+	EndedAt            float32  `json:"ended_at"`
+	UpdatedAt          float32  `json:"updated_at"`
+	Price              int      `json:"price"`
+	CurrencyCode       string   `json:"currency_code"`
+	Tags               []string `json:"tags"`
+	Materials          []string `json:"materials"`
+	Views              int      `json:"views"`
+	ItemWeight         int      `json:"item_weight"`
+	ItemWeightUnit     string   `json:"item_weight_unit"`
+	ItemLength         int      `json:"item_length"`
+	ItemWidth          int      `json:"item_width"`
+	ItemHeight         int      `json:"item_height"`
+	ItemDimensionsUnit string   `json:"item_dimensions_unit"`
+	Recipient          string   `json:"recipient"`
 }
 
 func (r *Route) getItems(ctx *gin.Context) {
