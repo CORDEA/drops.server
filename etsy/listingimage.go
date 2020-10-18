87d25e8e3cd4c57685525e5386f3b8dc1c7dcfb1
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-18 10:15:59 +0900

    get images

--- etsy/listingimage.go
+++ etsy/listingimage.go
@@ -1,5 +1,12 @@
 package etsy
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
 type ListingImages struct {
 	Count   int            `json:"count"`
 	Results []ListingImage `json:"results"`
@@ -8,4 +15,24 @@ type ListingImages struct {
 type ListingImage struct {
 	Id       string `json:"id"`
 	ImageUrl string `json:"url_570xN"`
+}
+
+func (c *Client) FindImages(id string) (*ListingImages, error) {
+	u, err := url.Parse(BaseUrl + "/listings/" + id + "/images")
+	if err != nil {
+		return nil, err
+	}
+	values := u.Query()
+	values.Add("api_key", c.key)
+	response, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	var images ListingImages
+	if err := json.Unmarshal(body, &images); err != nil {
+		return nil, err
+	}
+	return &images, nil
 }
