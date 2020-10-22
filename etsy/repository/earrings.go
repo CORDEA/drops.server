9d9821762e5c2b385726905a9e642aa9362c5257
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-22 09:30:05 +0900

    enable cache

--- etsy/repository/earrings.go
+++ etsy/repository/earrings.go
@@ -2,18 +2,31 @@ package repository
 
 import (
 	"github.com/CORDEA/drops.server/etsy/api"
+	"github.com/patrickmn/go-cache"
 )
+
+const EarringsCacheKey = "earrings"
 
 type EarringsRepository struct {
 	client *api.Client
+	cache  *cache.Cache
 }
 
-func NewEarringsRepository(client *api.Client) *EarringsRepository {
-	return &EarringsRepository{client}
+func NewEarringsRepository(client *api.Client, cache *cache.Cache) *EarringsRepository {
+	return &EarringsRepository{client, cache}
 }
 
 func (r *EarringsRepository) FindAll() (*api.Listings, error) {
-	return r.client.FindActiveEarrings()
+	if c, found := r.cache.Get(EarringsCacheKey); found {
+		return c.(*api.Listings), nil
+	}
+	res, err := r.client.FindActiveEarrings()
+	if err == nil {
+		r.cache.Set(EarringsCacheKey, res, cache.DefaultExpiration)
+	} else {
+		r.cache.Delete(EarringsCacheKey)
+	}
+	return res, err
 }
 
 func (r *EarringsRepository) FindImages(id string) (*api.ListingImages, error) {
--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"github.com/CORDEA/drops.server/items"
 	"github.com/CORDEA/drops.server/user"
 	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
 	"os"
+	"time"
 )
 
 var router = gin.Default()
@@ -15,7 +17,8 @@ var router = gin.Default()
 func main() {
 	key := os.Getenv("ETSY_API_KEY")
 	client := api.NewClient(key)
-	earringsRepo := repository.NewEarringsRepository(client)
+	c := cache.New(1*time.Hour, 30*time.Minute)
+	earringsRepo := repository.NewEarringsRepository(client, c)
 	getEarrings := usecase.NewGetEarrings(earringsRepo)
 
 	user.NewRoute(getEarrings).Apply(router)
