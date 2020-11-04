aaed2a5293fd2fe996eabafbf1848e39e67d5c33
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-04 10:28:49 +0900

    return time

--- user/orders.go
+++ user/orders.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
+	"time"
 )
 
 type Status int
@@ -34,6 +35,10 @@ type Order struct {
 	Status       string       `json:"status"`
 	Items        []model.Item `json:"items"`
 	IsCancelable bool         `json:"is_cancelable"`
+	OrderedAt    *time.Time   `json:"ordered_at"`
+	ShippedAt    *time.Time   `json:"shipped_at"`
+	DeliveredAt  *time.Time   `json:"delivered_at"`
+	CanceledAt   *time.Time   `json:"canceled_at"`
 }
 
 func (r *Route) getOrders(ctx *gin.Context) {
@@ -48,22 +53,33 @@ func (r *Route) getOrders(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	now := time.Now()
 	orders = append(orders, Order{
 		Id:           id.String(),
 		Status:       Confirmed.String(),
 		Items:        earrings.Items[0:3],
 		IsCancelable: true,
+		OrderedAt:    &now,
+		ShippedAt:    nil,
+		DeliveredAt:  nil,
+		CanceledAt:   nil,
 	})
 	id, err = uuid.NewUUID()
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	orderedAt := now.Add(-5 * 24 * time.Hour)
+	shippedAt := now.Add(-2 * 24 * time.Hour)
 	orders = append(orders, Order{
 		Id:           id.String(),
 		Status:       Delivered.String(),
 		Items:        earrings.Items[4:5],
 		IsCancelable: false,
+		OrderedAt:    &orderedAt,
+		ShippedAt:    &shippedAt,
+		DeliveredAt:  &now,
+		CanceledAt:   nil,
 	})
 	ctx.JSON(
 		http.StatusOK,
