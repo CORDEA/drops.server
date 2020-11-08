fa7e9306f6605adb11fec1bb8000adc77a2a6d0f
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-08 14:27:51 +0900

    change response

--- user/login.go
+++ user/login.go
@@ -2,9 +2,7 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"net/http"
-	"time"
 )
 
 type body struct {
@@ -18,21 +16,7 @@ func (r *Route) login(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	id, err := uuid.NewUUID()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	ctx.JSON(http.StatusOK, User{
-		Id:          int64(id.ID()),
-		Name:        "name",
-		Description: "description",
-		ImageUrl:    "",
-		City:        "Tokyo",
-		Rank:        1,
-		CreatedAt:   time.Time{},
-		Birthday:    time.Time{},
-	})
+	ctx.String(http.StatusOK, "")
 }
 
 func (r *Route) signUp(ctx *gin.Context) {
@@ -41,19 +25,5 @@ func (r *Route) signUp(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	id, err := uuid.NewUUID()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	ctx.JSON(http.StatusOK, User{
-		Id:          int64(id.ID()),
-		Name:        "name",
-		Description: "description",
-		ImageUrl:    "",
-		City:        "Tokyo",
-		Rank:        1,
-		CreatedAt:   time.Time{},
-		Birthday:    time.Time{},
-	})
+	ctx.String(http.StatusOK, "")
 }
